5373003d63521f6049a17e5fb5c13cdf62483b32
likithmanoj <likithmanojm@gmail.com>
2026-07-19 15:35:36 +0530

    feat(iam): Implemented STS policy for the iam user instead of giving S3 permissions and removed the older policy with S3 permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,30 @@ resource "aws_iam_access_key" "nhi_runner_keys" {
 
 data "aws_partition" "current" {}
 
-resource "aws_iam_policy" "policy" {
+# resource "aws_iam_policy" "policy" {
+#   name        = "${var.project_name}-${var.environment}-s3-policy"
+#   path        = "/"
+#   description = "IAM policy for automation runner to access S3 buckets in ${var.environment} environment"
+
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         # Allow the automation runner to perform S3 actions on the specified bucket when Python is run we need GetObject and PutObject permissions to read and write files to the S3 bucket
+#         Action = ["s3:ListBucket", "s3:GetObject", "s3:PutObject"]
+#         Effect = "Allow"
+#         Resource = [
+#           "arn:${data.aws_partition.current.partition}:s3:::${var.project_name}-${var.environment}-bucket",
+#           "arn:${data.aws_partition.current.partition}:s3:::${var.project_name}-${var.environment}-bucket/*"
+#         ]
+#       }
+#     ]
+#   })
+# } Replaced the entire code with below policy to remove the IAM user credential and give it least privilege to the IAM user by creating a role and let the iam policy to attach to that role as in fit into that role
+
+resource "aws_iam_policy" "nhi_user_sts_policy" {
   name        = "${var.project_name}-${var.environment}-s3-policy"
   path        = "/"
   description = "IAM policy for automation runner to access S3 buckets in ${var.environment} environment"
@@ -34,13 +57,10 @@ resource "aws_iam_policy" "policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        # Allow the automation runner to perform S3 actions on the specified bucket when Python is run we need GetObject and PutObject permissions to read and write files to the S3 bucket
-        Action = ["s3:ListBucket", "s3:GetObject", "s3:PutObject"]
+        #Removed S3 actions to maintain least privilege to the Python user and resource is now pointing to role created.
+        Action = ["sts:AssumeRole"]
         Effect = "Allow"
-        Resource = [
-          "arn:${data.aws_partition.current.partition}:s3:::${var.project_name}-${var.environment}-bucket",
-          "arn:${data.aws_partition.current.partition}:s3:::${var.project_name}-${var.environment}-bucket/*"
-        ]
+        Resource = aws_iam_role.nhi_automation_runner_role.arn
       }
     ]
   })
@@ -48,7 +68,7 @@ resource "aws_iam_policy" "policy" {
 
 resource "aws_iam_user_policy_attachment" "nhi_runner_policy_attachment" {
   user       = aws_iam_user.nhi_automation_runner.name
-  policy_arn = aws_iam_policy.policy.arn
+  policy_arn = aws_iam_policy.nhi_user_sts_policy.arn
 }
 
 resource "aws_s3_bucket" "nhi_automation_bucket" {
@@ -58,6 +78,17 @@ resource "aws_s3_bucket" "nhi_automation_bucket" {
   tags = {
     Name        = "${var.project_name}-${var.environment}-bucket"
     Environment = var.environment
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_resource"{
+  bucket = aws_s3_bucket.nhi_automation_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
   }
 
 }
